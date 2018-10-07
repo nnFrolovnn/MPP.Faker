558191b6a2e1f93494796298340af68ed19bd661
Vladislav Frolov <37739674+nnFrolovnn@users.noreply.github.com>
2018-10-07 22:30:23 +0300

    Changes in Faker

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -12,20 +12,34 @@ namespace Faker
     public class Faker
     {
         readonly ConcurrentDictionary<Type, IGenerator> registredTypes;
+        readonly ConcurrentDictionary<Type, int> currentrecursionLevel;
+        int maxRecursionLevel;
         readonly Stack<Type> usedTypes;
-        object locker; 
+        object locker;
 
         public Faker(ConcurrentDictionary<Type, IGenerator> dictionary)
         {
             registredTypes = dictionary;
             usedTypes = new Stack<Type>();
             locker = new object();
+            maxRecursionLevel = 1;
+            currentrecursionLevel = new ConcurrentDictionary<Type, int>();
+        }
+
+        public Faker(ConcurrentDictionary<Type, IGenerator> dictionary, int recursionLevel)
+        {
+            registredTypes = dictionary;
+            usedTypes = new Stack<Type>();
+            locker = new object();
+            maxRecursionLevel = recursionLevel;
+            currentrecursionLevel = new ConcurrentDictionary<Type, int>();
         }
 
         public T Create<T>() where T : new()
         {
-            var constructors = (typeof(T).GetConstructors().OrderBy(x => x.GetParameters().Length)).ToArray();
-            
+            //TODO stack
+            var constructors = (typeof(T).GetConstructors().OrderByDescending(x => x.GetParameters().Length)).ToArray();
+
             bool isCreated = false;
             int constructorNumber = 1;
             T result = default(T);
@@ -37,6 +51,7 @@ namespace Faker
                     var useConstructor = constructors[constructorNumber - 1];
                     result = CreatefromConstructor<T>(useConstructor);
                     isCreated = true;
+
                 }
                 catch
                 {
@@ -44,7 +59,7 @@ namespace Faker
                     constructorNumber++;
                 }
             }
-            
+
             if (isCreated)
             {
                 FillProperties(ref result);
@@ -54,6 +69,38 @@ namespace Faker
             else
             {
                 throw new Exception("can't create object (Type: " + typeof(T).FullName + ")");
+            }
+        }
+
+        public bool HaveToCreate(Type type)
+        {
+            if (usedTypes.Count == 0)
+            {
+                return true;
+            }
+            else if (usedTypes.Contains(type))
+            {
+                if (currentrecursionLevel[type] < maxRecursionLevel)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void AddToStack(Type type)
+        {
+            //TODO
+            if(usedTypes.Contains(type))
+            {
+                currentrecursionLevel[type] += 1;
             }
         }
 
@@ -80,8 +127,6 @@ namespace Faker
                     if (property.SetMethod.IsPublic)
                     {
                         SetProperty(ref result, property);
-
-                        //property.SetMethod.Invoke(result, new[] {GetValue(property.PropertyType)});
                     }
                 }
             }
@@ -98,7 +143,7 @@ namespace Faker
                 }
                 else
                 {
-                    return new object();
+                    return GetDTO(type);
                 }
             }
             catch
@@ -115,11 +160,11 @@ namespace Faker
 
             if (registredTypes.ContainsKey(propertyType))
             {
-                set = new object[] { registredTypes[propertyType].Generate() };            
+                set = new object[] { registredTypes[propertyType].Generate() };
             }
             else
             {
-                set = new object[] { GetDTO(ref result, propertyType) };
+                set = new object[] { GetDTO(propertyType) };
             }
 
             property.SetMethod.Invoke(result, set);
@@ -127,19 +172,19 @@ namespace Faker
         }
 
 
-        private object GetDTO<T>(ref T result, Type property)
+        private object GetDTO(Type type)
         {
             try
             {
-                MethodInfo method = typeof(Faker).GetMethod("Create", BindingFlags.Public);
-                MethodInfo genericMethod = method?.MakeGenericMethod();
-                var dto = genericMethod?.Invoke(this, new object[] { result });
+                MethodInfo method = typeof(Faker).GetMethod("Create");
+                MethodInfo genericMethod = method?.MakeGenericMethod(type);
+                var dto = genericMethod?.Invoke(this, null);
                 return dto;
             }
             catch
             {
                 return null;
-            }  
+            }
         }
     }
 }
--- FakerApp/Program.cs
+++ FakerApp/Program.cs
@@ -14,14 +14,31 @@ namespace FakerApp
             ConcurrentDictionary<Type, IGenerator> dictionary = plugin.GetGenerators();
             Faker.Faker faker = new Faker.Faker(plugin.GetGenerators());
 
-            Foo foo = faker.Create<Foo>();
+            Sor t = faker.Create<Sor>();
 
-
+            Console.WriteLine(t.ToString());
             Console.ReadKey();
         }
     }
 
+    public class Sor
+    {
+        public enum TTR
+        {
+            TT, RR, EE, WW
+        }
 
+        public Foo foo { get; private set; }
+        public Bar bar { protected get; set; }
+
+        public Sor(Foo foo, TTR ttr) { }
+        public Sor() { }
+
+        public override string ToString()
+        {
+            return ("{TypeName: " + typeof(Sor).Name + "}");
+        }
+    }
 
     public class Foo
     {
@@ -30,8 +47,30 @@ namespace FakerApp
 
         public int I { get; set; }
 
-        public string K { get; }
+        public string K { get; set; }
         private double R { get; set; }
-        public string FF { set => gg = value; }
+        public string FF { get => gg; set => gg = value; }
+
+        public override string ToString()
+        {
+            return ("{TypeName: " + typeof(Foo).Name + "}");
+        }
+    }
+
+    public class Bar
+    {
+        public Foo foo { get; set; }
+
+        public Bar(Foo foo, int tt, string gg)
+        {
+
+        }
+
+        public Bar() { }
+
+        public override string ToString()
+        {
+            return ("{TypeName: " + typeof(Bar).Name + "}");
+        }
     }
 }
